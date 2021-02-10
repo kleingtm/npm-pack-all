bd7851579366a34c8c2ee829d7f633cb4c8f014f
Tom Kleingers <kleingtm@gmail.com>
2021-02-10 17:46:23 -0500

    NPA-9: TKLEING - Handle scoped npm packages

--- mocks/shelljs.mocks.js
+++ mocks/shelljs.mocks.js
@@ -6,7 +6,6 @@ const defaultResult = {
 
 module.exports.mockShellFn = function(fnName, result = defaultResult) {
     return jest.fn(function() {
-        console.info(`  + shell.${fnName}(${String([...arguments])})`);
         return result;
     });
 };
